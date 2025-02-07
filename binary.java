1db7145ae445f151e68c63f1ec66c5e1b3cf00d8
kodurisaivishnu <129348459+kodurisaivishnu@users.noreply.github.com>
2025-02-07 16:40:28 +0530

    Create binary.java

---
+++ binary.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class binary {
+  public static void sol(ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> temp, int n) {
+    if (temp.size() == n) {
+      ans.add(new ArrayList<>(temp));
+    } else {
+      for (int i = 0; i < 2; i++) { // numeber of choces can vary on 2 = n
+        temp.add(i);
+        sol(ans, temp, n);
+        temp.remove(temp.size() - 1);
+      }
+    }
+  }
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number: ");
+    int n = sc.nextInt();
+    ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+    sol(ans, new ArrayList<>(), n);
+    System.out.println(ans);
+  }
+}
